d4b130754f676dda1ce8565631ff4f778c4a7c0b
Kush Joshi <76556985+KushJoshi16@users.noreply.github.com>
2023-06-11 12:37:24 +0530

    Added frontend templates and routes

--- application.py
+++ application.py
@@ -1,4 +1,16 @@
-from flask import Flask, jsonify, request, Response, abort
+from flask import (
+    Flask, 
+    jsonify, 
+    request, 
+    Response, 
+    redirect,
+    abort, 
+    g,
+    render_template,
+    session,
+    url_for,
+    flash
+)
 import pandas as pd
 import generator_algo.algorithm as alg
 import generator_algo.output_format as outFmat
@@ -8,21 +20,113 @@ import os
 from flask_cors import CORS
 
 
+class User:
+
+    def __init__(self, id, username, password) -> None:
+        self.id = id
+        self.username = username
+        self.password = password
+        
+    def __repr__(self) -> str:
+        return f'<User : {self.username}>'
+
+admins = []
+admins.append(User(id=1,username='admin',password='admin'))
+admins.append(User(id=2,username='admin1',password='admin1'))
+
+
 logger = logging.getLogger('azure.mgmt.resource')
  
 application = Flask(__name__)
+application.secret_key = 'MYsecretKEy'
 CORS(application)
 app = application
 
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+       user = [x for x in admins if x.id == session['user_id']]
+       if user:
+           g.user = user[0]
+
 
 @app.route('/')
-def Hello_page():
-#     app.logger.debug('This is a debug log message')
-#     app.logger.info('This is an information log message')
-#     app.logger.warn('This is a warning log message')
-#     app.logger.error('This is an error message')
-#     app.logger.critical('This is a critical message')
-  return 'Hello, The Website is fine!!'
+def index():
+    day = [
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday'
+        ]
+    with open('classes/TimeTable.json',"r") as f:
+        data = f.read()
+    return render_template('index.html', title="page",len = len(day),daylen = alg.dt.CLASS_HOURS,day = day, jsonfile=json.dumps(data))
+
+
+@app.route('/admin')
+def admin():
+    if not g.user:
+        return redirect(url_for('login'))
+    return render_template('admin_page.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method =='POST':
+        session.pop('user_id',None)
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = [x for x in admins if x.username == username]
+        if user and user[0].password == password:
+            session['user_id'] = user[0].id
+            flash('You were successfully logged in')
+            return redirect(url_for('admin'))
+        else:
+            flash('Invalid credentials')
+        return redirect(url_for('login'))
+        
+    return render_template('login.html')
+
+
+
+@app.route('/setinputData',methods=['POST'])
+def setinputData():
+    filepath = None
+    if 'file' not in request.files:
+        
+        flash('Error')
+        return redirect(url_for('admin'))
+    
+    f = request.files['file']
+    basepath = os.path.dirname(__file__)
+    filepath = os.path.join(basepath,'uploads',f.filename)
+    f.save(filepath)
+    logger.info("Input data received")
+    try:
+        data = None
+        with open('classes/input.json') as f:
+            data = json.load(f)
+            
+        df = pd.read_csv(filepath)
+        df = df.loc[df.astype(str).drop_duplicates().index]
+        data['Lectures'] = df.to_dict(orient='records')
+        with open('classes/input.json','w') as f:
+            json.dump(data,f)
+
+        logger.info("Input data stored in input.json")
+
+        flash('Input Saved')
+        return redirect(url_for('admin'))
+    except Exception as e:
+        logger.error(e)
+        flash('Error')
+        return redirect(url_for('admin'))
+
+
 
 @app.route('/set_inputData',methods=['POST'])
 def set_inputData():
@@ -53,12 +157,21 @@ def set_inputData():
         abort(500)
 
  
-@app.route('/generate_time_table')
-def generate_time_table():
+@app.route('/generatetimetable')
+def generatetimetable():
     alg.evolutionary_algorithm()
     outFmat.timeTableFromOutput()
     logger.info("Time Table Generated")
     return jsonify(success=True)
+ 
+@app.route('/generatetimetable')
+def generate_time_table():
+    alg.evolutionary_algorithm()
+    outFmat.timeTableFromOutput()
+    logger.info("Time Table Generated")
+    print("Reached")
+    flash('Time Table Genration complete')
+    return redirect(url_for('admin'))
 
 @app.route('/get_time_table')
 def get_time_table():
--- generator_algo/output_format.py
+++ generator_algo/output_format.py
@@ -8,25 +8,25 @@ def timeTableFromOutput():
     with open('classes/output.json') as f:
         data = json.load(f)
     day = [
-        "monday",
-        "tuesday",
-        "wednesday",
-        "thursday",
-        "friday",
-        "saturday"
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday'
         ]
-    Time_table = {"sections":{}}
+    Time_table = {'sections':{}}
     for record in data:
         for group in eval(record['Group']):
             section = Time_table['sections'].get(group,None)
             if not section:
                 section = dict({
-                    "monday":[""]*class_hours,
-                    "tuesday":[""]*class_hours,
-                    "wednesday":[""]*class_hours,
-                    "thursday":[""]*class_hours,
-                    "friday":[""]*class_hours,
-                    "saturday":[""]*class_hours
+                    'monday':['']*class_hours,
+                    'tuesday':['']*class_hours,
+                    'wednesday':['']*class_hours,
+                    'thursday':['']*class_hours,
+                    'friday':['']*class_hours,
+                    'saturday':['']*class_hours
                     })
             
             sub_day = day[int(record['Assigned_time'])//class_hours]
@@ -39,3 +39,7 @@ def timeTableFromOutput():
             
     with open('classes/TimeTable.json','w') as f:
         json.dump(Time_table,f)
+
+
+if __name__ == '__main__':
+    timeTableFromOutput()
